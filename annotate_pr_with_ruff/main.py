5ab3782e85d2882a21d7d18d03b2a7fbe7523821
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-02-27 13:48:51 +0100

    :bug: use GITHUB_OUTPUT

--- annotate_pr_with_ruff/main.py
+++ annotate_pr_with_ruff/main.py
@@ -6,6 +6,11 @@ from github_utils import get_diff
 from ruff_utils import ruff
 
 
+def set_output(name: str, value: str):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
+
+
 def main():
     with open(os.environ["GITHUB_EVENT_PATH"]) as f:
         event_data = json.load(f)
@@ -16,11 +21,11 @@ def main():
     # Check which files have changed
     diff = get_diff(owner=owner, repo=repo, pr_number=pr_number)
     changed_files = get_changed_files(diff)
-    print(f"{changed_files=}")
+    # print(f"{changed_files=}")
 
     # Get errors from ruff
     ruff_errors = ruff()
-    print(f"{ruff_errors=}")
+    # print(f"{ruff_errors=}")
 
     # Filter only errors on lines that have been changed
     ruff_errors = [
@@ -28,12 +33,14 @@ def main():
         for ruff_error in ruff_errors
         if f"/{ruff_error.file}" in changed_files
     ]
-    print(f"::set-output name=num_errors::{len(ruff_errors)}")
+    # print(f"::set-output name=num_errors::{len(ruff_errors)}")
+    set_output("num_errors", str(len(ruff_errors)))
 
     # Submit review
     for ruff_error in ruff_errors:
         print(
             f"::error file={ruff_error.file},line={ruff_error.line_number}::{ruff_error.message}"
+
         )
 
 
